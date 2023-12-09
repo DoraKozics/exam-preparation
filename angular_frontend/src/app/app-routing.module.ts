1709dd9552a46a6a467fced00b80b3dec009a8e3
kozic <kozicsdora@gmail.com>
2023-12-09 23:26:33 +0100

    add styling

--- angular_frontend/src/app/app-routing.module.ts
+++ angular_frontend/src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {BookListComponent} from "./components/book-list/book-list.component";
+import {BookFormComponent} from "./components/book-form/book-form.component";
+import {Error404Component} from "./components/error404/error404.component";
+import {BookSearchComponent} from "./components/book-search/book-search.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: BookListComponent},
+  {path: 'list', component: BookListComponent},
+  {path: 'form', component: BookFormComponent},
+  {path: 'search', component: BookSearchComponent},
+  {path: '**', component: Error404Component}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- angular_frontend/src/app/app.module.ts
+++ angular_frontend/src/app/app.module.ts
@@ -4,11 +4,19 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
+import { BookFormComponent } from './components/book-form/book-form.component';
+import { BookListComponent } from './components/book-list/book-list.component';
+import { Error404Component } from './components/error404/error404.component';
+import { BookSearchComponent } from './components/book-search/book-search.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavbarComponent
+    NavbarComponent,
+    BookFormComponent,
+    BookListComponent,
+    Error404Component,
+    BookSearchComponent
   ],
   imports: [
     BrowserModule,
